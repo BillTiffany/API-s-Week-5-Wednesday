a022b6144e9a39226dc4af723790c808e744c018
Bill Tiffany <daganlegalservice@gmail.com>
2021-10-13 16:24:17 -0600

    uploading finished project

---
+++ main.js
@@ -0,0 +1,38 @@
+// document.getElementById("residents").addEventListener("click", function() {
+    // document.getElementById("demo").innerHTML = "Button Clicked";
+//   });
+
+
+const pDemo = document.getElementById('demo')
+const btn = document.querySelector("#aresidents")
+
+function click(){
+    console.log("Button Clicked")
+    axios.get("https://swapi.dev/api/planets/2")
+    .then(response => {
+        console.log(response.data)
+        console.log(response.data.residents)
+        for(let i=0; i<response.data.residents.length; i++){
+            axios.get(response.data.residents[i])
+            .then(res =>{
+               const person = document.createElement("li")
+               person.textContent=(res.data.name)
+               pDemo.appendChild(person)
+            console.log(res.data.name)
+                    
+                })
+                // console.log(response.data.residents[i])
+            }
+
+          })
+          .catch(error => {
+            console.log(error)
+          })
+        
+    
+}
+
+document.getElementById("aresidents").addEventListener("click", click)
+
+
+  
